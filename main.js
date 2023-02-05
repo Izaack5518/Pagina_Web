6b8a1e191b355b99e0e2ad0d4c8c25b4891d3daf
Izaack5518 <118235590+Izaack5518@users.noreply.github.com>
2023-02-05 14:00:21 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const lista = document.getElementById('lista');
+const li = document.createElement('li');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
+
+li.textContent = 'producto-A'
+li2.textContent = 'Producto-b'
+li3.textContent = 'producto-A'
+lista.appendChild(li)
+lista.appendChild(li2)
+lista.appendChild(li3)
+
+
